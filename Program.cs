1c9e400a24b2b3691f3572d2bbde660ef0ed07a0
hermansapitri00 <hermansapitri00@gmail.com>
2024-03-15 17:51:43 +0700

    i just want to test thing

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text.Json;
+
+var data = new StreamReader("Data/PokemonList.json").BaseStream;
+var pokemons =
+    await JsonSerializer.DeserializeAsync<PokemonApi>(
+        data,
+        new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+    ) ?? throw new Exception("Failed to deserialize Pokemon list");
+
+foreach (var pokemon in pokemons.Results)
+{
+    Console.WriteLine(pokemon);
+}
+
+// Console.WriteLine(pokemons.Results);
+Console.ReadLine();
+
+record PokemonApi(int Count, string Next, string Previous, Pokemon[] Results);
+
+record Pokemon(string Name, string Url);
